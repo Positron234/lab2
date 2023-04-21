ef7498b24b83e7f2f622f07d9947aeb47fe47a6d
Positron234 <andreigaivoronskikh120@gmail.com>
2023-04-21 18:41:54 +0700

    Создан текст

---
+++ Schet/Schet/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace program
+{ 
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            Console.WriteLine("ввести А");
+            Console.WriteLine("ввести В");
+            Console.WriteLine("выполнить операцию “+”");
+            Console.WriteLine("выполнить операцию “-”");
+            Console.WriteLine("выполнить операцию “*”");
+            Console.WriteLine("выполнить операцию “/”");
+        }
+
+    }
+}
