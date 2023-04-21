9c4655639a8ba321f39f76e05b66cead0bf7844e
Positron234 <andreigaivoronskikh120@gmail.com>
2023-04-21 18:46:01 +0700

    function1

--- Schet/Schet/Program.cs
+++ Schet/Schet/Program.cs
@@ -5,12 +5,41 @@ namespace program
     {
         static void Main(string[] Args)
         {
+            int a = 0, b = 0;
+            string c;
             Console.WriteLine("ввести А");
+            a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("ввести В");
+            b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("выполнить операцию “+”");
             Console.WriteLine("выполнить операцию “-”");
             Console.WriteLine("выполнить операцию “*”");
             Console.WriteLine("выполнить операцию “/”");
+            c = Console.ReadLine();  
+            switch (c)
+            {
+                case ("*"):
+                    {
+                        Console.WriteLine(a * b); break;
+                    }
+                case ("/"):
+                    {
+                        Console.WriteLine(a / b); break;
+                    }
+                case ("+"):
+                    {
+                        Console.WriteLine(a + b); break;
+                    }
+                case ("-"):
+                    {
+                        Console.WriteLine(a - b); break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+
+            }
         }
 
     }
